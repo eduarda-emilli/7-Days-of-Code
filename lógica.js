5d1ccb6ca722a74c0090ae600a1db9049f4362ec
Eduarda Emilli <107655524+eduarda-emilli@users.noreply.github.com>
2022-11-23 19:31:38 -0300

    Create lógica.js

---
+++ lógica.js
@@ -0,0 +1,31 @@
+let numeroDez = 10
+let stringDez = '10'
+let numeroTrinta = 30
+let stringTringa = '30'
+let numeroVinte = 20
+let stringVinte = '20'
+
+if (numeroDez == stringDez) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+    console.log('As variáveis numeroDez e stringUm não tem o mesmo valor')
+}
+
+/* if (COMPARAR O numeroUm e a stringUm) {
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+  } else {
+    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+  }
+  
+  if (COMPARAR O numeroTrinta e a stringTrinta) {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+  } else {
+    console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+  }
+  
+  if (COMPARAR O numeroDez e a stringDez) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+  } else {
+    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+  }
+  */
