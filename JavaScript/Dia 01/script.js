bd6f69fd817f0e5b366897e163e5a861b6e66155
Eduarda Emilli <107655524+eduarda-emilli@users.noreply.github.com>
2022-11-23 19:56:45 -0300

    Update script.js

---
+++ JavaScript/Dia 01/script.js
@@ -0,0 +1,24 @@
+let numeroDez = 10
+let stringDez = '10'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroVinte = 20
+let stringVinte = '20'
+
+if (numeroDez == stringDez) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+    console.log('As variáveis numeroDez e stringUm não tem o mesmo valor')
+}
+
+if (numeroTrinta == stringTrinta) {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor, mas tipos diferentes')
+} else {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor, mas tipos diferentes')
+}
+
+if(numeroVinte == stringVinte) {
+    console.log('As variáveis numeroVinte e stringVinte tem o mesmo valor, mas tipos diferentes')
+} else {
+    console.log('As variáveis numeroVinte e stringVinte tem o mesmo valor, mas tipos diferentes')
+}
