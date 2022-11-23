f325b71eb9543ebfb22f27b90eae79e469689964
Eduarda Emilli <107655524+eduarda-emilli@users.noreply.github.com>
2022-11-23 19:37:23 -0300

    Operadores Lógicos
    Dia 01

---
+++ JavaScript/Dia 01/script.js
@@ -0,0 +1,12 @@
+let numeroDez = 10
+let stringDez = '10'
+let numeroTrinta = 30
+let stringTringa = '30'
+let numeroVinte = 20
+let stringVinte = '20'
+
+if (numeroDez == stringDez) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+    console.log('As variáveis numeroDez e stringUm não tem o mesmo valor')
+}
