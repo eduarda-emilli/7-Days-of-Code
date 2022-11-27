670f1a6f0e57438d3e7858bcdc798849367b695f
Eduarda Emilli <107655524+eduarda-emilli@users.noreply.github.com>
2022-11-26 21:03:46 -0300

    atualizei o index

---
+++ JavaScript/Dia 03/script.js
@@ -0,0 +1,52 @@
+var pergunta1 = document.querySelector('.pergunta1')
+var 
+
+
+
+
+
+
+/* var question = document.querySelector('.question')
+var option = document.querySelector('.option')
+var button = document.querySelector('.buttons')
+
+var front = document.getElementsByName('front')
+
+function front() {
+
+    var frontend = document.getElementsByName('front')
+    question.innerHTML = 'Qual tecnologia você está aprendendo?'
+
+    if (question == 'front-end') {
+        option.innerHTML += `<input type="radio" name="web" id="react">
+    <label for="react">React</label>
+    <input type="radio" name="web" id="vue">
+    <label for="vue">Vue</label>`
+    }
+
+    
+}
+ */
+
+
+/*  var fse = document.getElementsByName('radsex')
+var idade = ano - Number(fano.value)
+var gênero = ''
+var img = document.createElement('img')
+img.setAttribute('id', 'foto')
+if (fsex[0].checked) {
+    gênero = 'Homem' 
+    if (idade >= 0 && idade < 10) {
+       // Criança
+       img.setAttribute('src','foto-bebe-m.png')
+    } else if (idade < 21) {
+        // Jovem
+        img.setAttribute('src','foto-jovem-m.png')
+    } else if (idade < 50 ) {
+        // Adulto
+        img.setAttribute('src','foto-adulto-m.png')
+    } else {
+        // Idoso
+        img.setAttribute('src','foto-idoso-m.png')
+    }
+    */
